c6c5eb6470a2a58bb9e3f29ecbec90cf302cfaaf
robyafrizal <robyafrizal86@gmail.com>
2019-11-15 15:59:37 +0700

    Updates

---
+++ precache-manifest.3d9ad76834780aef2bfff90ddd8cdd5c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "155c40dca0d0a2821c699be127ed1b95",
+    "url": "/project-react-counter/index.html"
+  },
+  {
+    "revision": "d011a812b4aa0e3cf1b2",
+    "url": "/project-react-counter/static/css/main.cb5baa06.chunk.css"
+  },
+  {
+    "revision": "725070c98b4e64b27696",
+    "url": "/project-react-counter/static/js/2.e27b871b.chunk.js"
+  },
+  {
+    "revision": "d011a812b4aa0e3cf1b2",
+    "url": "/project-react-counter/static/js/main.91339a0d.chunk.js"
+  },
+  {
+    "revision": "a4ef2ba1f3fdb534f331",
+    "url": "/project-react-counter/static/js/runtime-main.30eee51c.js"
+  },
+  {
+    "revision": "d1bf319ac0691f56aa0f3b473e6bef92",
+    "url": "/project-react-counter/static/media/image1.d1bf319a.jpeg"
+  }
+]);
